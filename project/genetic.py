9e0438f43b0675d65c38bf75864a514d60abdd18
Shahbaz Jahan <shahbazjahan9@gmail.com>
2024-01-21 23:20:23 +0530

    genetic initial commit

---
+++ project/genetic.py
@@ -0,0 +1,143 @@
+import yfinance as yf
+import datetime as dt
+import random
+from collections import defaultdict 
+
+from project.indicator import MFI, RSI, MA, BBANDS
+
+def generation(rsi,mfi,bbands,sma,ewma,learning_rate):
+    def fetch_weights():
+        file = open("weights.txt", "r")
+        lst=file.readlines()
+        dictionary=defaultdict()
+        dictionary["rsi_parameter"]=float(lst[0])
+        dictionary["mfi_parameter"]=float(lst[1])
+        dictionary["ma_parameter"]=float(lst[2])
+        dictionary["bbands_parameter"]=float(lst[3])
+        dictionary["capital_percentage"]=float(lst[4])
+        dictionary["trade_threshold"]=float(lst[5])
+        dictionary["remaining_capital"]=1000
+        dictionary["starting_capital"]=1000
+        file.close
+        return dictionary
+    def populate(weights):
+        num_of_players=20
+        population=[weights]
+        new_weights=defaultdict()
+        for i in range(num_of_players):
+            new_weights.clear()
+            rand_num=random.randint(-1000,1000)
+            new_weights["rsi_parameter"]=weights["rsi_parameter"]+rand_num*learning_rate/1000
+            rand_num=random.randint(-1000,1000)
+            new_weights["mfi_parameter"]=weights["mfi_parameter"]+rand_num*learning_rate/1000
+            rand_num=random.randint(-1000,1000)
+            new_weights["ma_parameter"]=weights["ma_parameter"]+rand_num*learning_rate/1000
+            rand_num=random.randint(-1000,1000)
+            new_weights["bbands_parameter"]=weights["bbands_parameter"]+rand_num*learning_rate/1000
+            rand_num=random.randint(-1000,1000)
+            new_weights["capital_percentage"]=weights["capital_percentage"]+rand_num*learning_rate/1000
+            rand_num=random.randint(-1000,1000)
+            new_weights["trade_threshold"]=weights["trade_threshold"]+rand_num*learning_rate/1000
+            new_weights["remaining_capital"]=weights["starting_capital"]
+            new_weights["starting_capital"]=weights["starting_capital"]
+            population.append(new_weights)
+            # print(new_weights)
+
+        return population
+
+    def play_game(list_of_population,rsi,mfi,bbands,sma,ewma):
+        result_of_game=[]
+        for i in range(len(list_of_population)):
+            weights_of_individual=list_of_population[i]
+            individual_portfolio=defaultdict()
+            individual_portfolio["remaining_capital"]=weights_of_individual["starting_capital"]
+            individual_portfolio["starting_capital"]=weights_of_individual["starting_capital"]
+            individual_portfolio["number_of_stock"]=0
+            for j in range(len(rsi)):
+                buy_sell_signal=rsi[j]*weights_of_individual["rsi_parameter"]
+                buy_sell_signal+=mfi[j]*weights_of_individual["mfi_parameter"]
+                buy_sell_signal+=(sma[j]-ewma[j]/ewma[j])*weights_of_individual["ma_parameter"]
+                buy_sell_signal+=((bbands["MiddleBand"][j]-bbands["Close"][j])/(bbands["UpperBand"][j]-bbands["LowerBand"][j]))*weights_of_individual["bbands_parameter"]
+                if(buy_sell_signal>abs(weights_of_individual["trade_threshold"])):
+                    if(individual_portfolio["remaining_capital"]>=(individual_portfolio["starting_capital"]*weights_of_individual["capital_percentage"])):
+                        trade_units=(individual_portfolio["starting_capital"]*weights_of_individual["capital_percentage"]*buy_sell_signal)//(bbands["Close"][j])
+                        individual_portfolio["remaining_capital"]-=trade_units*bbands["Close"][j]
+                        individual_portfolio["number_of_stock"]+=trade_units
+                if(buy_sell_signal<abs(weights_of_individual["trade_threshold"])*(-1)):
+                    if(individual_portfolio["number_of_stock"]>0):
+                        trade_units=(individual_portfolio["starting_capital"]*weights_of_individual["capital_percentage"]*buy_sell_signal)//(bbands["Close"][j])
+                        if(individual_portfolio["number_of_stock"]>=trade_units):
+                            individual_portfolio["number_of_stock"]-=trade_units
+                            individual_portfolio["remaining_capital"]+=trade_units*bbands["Close"][j]
+                        else:
+                            individual_portfolio["remaining_capital"]+=individual_portfolio["number_of_stock"]*bbands["Close"][j]
+                            individual_portfolio["number_of_stock"]=0
+            final_result_of_individual=individual_portfolio["remaining_capital"]+individual_portfolio["number_of_stock"]*bbands["Close"][-1]
+            result_of_game.append(final_result_of_individual)
+        return result_of_game                       
+    
+    def find_best(list_of_population,result_of_game):
+        maximum=max(result_of_game)
+        for i in range(len(result_of_game)):
+            if(result_of_game[i]==maximum):
+                return list_of_population[i]
+        return list_of_population[0]
+
+    def update_weights(new_weights,):
+        file = open("weights.txt","w")
+        l=[str(new_weights["rsi_parameter"])+"\n",str(new_weights["mfi_parameter"])+"\n",str(new_weights["ma_parameter"])+"\n",str(new_weights["bbands_parameter"])+"\n",str(new_weights["capital_percentage"])+"\n",str(new_weights["trade_threshold"])+"\n"]
+        file.writelines(l)
+        file.close()
+
+    weights=fetch_weights()
+    list_of_population=populate(weights)
+    result_of_game=play_game(list_of_population,rsi,mfi,bbands,sma,ewma)
+    new_weights=find_best(list_of_population,result_of_game)
+    update_weights(new_weights)
+    print("result=",max(result_of_game),new_weights["rsi_parameter"],new_weights["mfi_parameter"],new_weights["ma_parameter"],new_weights["bbands_parameter"],new_weights["capital_percentage"],new_weights["trade_threshold"],"lr=",learning_rate)
+
+# the below commented code can restart the training of the genetic algorithm
+# file = open("weights.txt","w")
+# l=["0\n","0\n","0\n","0\n","0\n","0\n"]
+# file.writelines(l)
+# file.close()
+start_date = dt.datetime.today()- dt.timedelta(3000) 
+end_date = dt.datetime.today()
+stock ="USDJPY=X"
+data = yf.download(stock, start_date, end_date)
+learning_rate=1
+# print(data["Open"].iloc[0])
+print(len(data))
+rsi=RSI(data)
+mfi=MFI(data)
+bbands=BBANDS(data)
+ma=MA(data)
+sma=ma[0]
+ewma=ma[1]
+print("RSI")
+print(len(rsi))
+print("MFI")
+print(len(mfi))
+print("BBANDS")
+print(len(bbands))
+print("MA")
+print(len(sma))
+print(len(ewma))
+smallest_length_data=len(ewma)
+rsi=rsi[len(rsi)-len(ewma):]
+mfi=mfi[len(mfi)-len(ewma):]
+bbands=bbands[len(bbands)-len(ewma):]
+sma=sma[len(sma)-len(ewma):]
+print("RSI")
+print(len(rsi))
+print("MFI")
+print(len(mfi))
+print("BBANDS")
+print(len(bbands))
+print("MA")
+print(len(sma))
+print(len(ewma))
+for i in range(1000):
+    print(i,end=" ")
+    generation(rsi,mfi,bbands,sma,ewma,learning_rate)
+    learning_rate=learning_rate*1
---
+++ project/indicator.py
@@ -0,0 +1,109 @@
+import pandas as pd
+import yfinance as yf
+import datetime as dt
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+
+# Calculate money flow index
+def MFI(data, n=14):
+    high=data['High']
+    low=data['Low']
+    close=data['Close']
+    tr = np.amax(np.vstack(((high - low).to_numpy(), (abs(high - close)).to_numpy(), (abs(low - close)).to_numpy())).T, axis=1)
+    return pd.Series(tr).rolling(n).mean().to_numpy()
+
+
+
+def RSI(data, periods = 14):
+    close=data['Close']
+    close_delta = close.diff()
+
+    # Make two series: one for lower closes and one for higher closes
+    up = close_delta.clip(lower=0)
+    down = -1 * close_delta.clip(upper=0)
+    
+    ma_up = up.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+    ma_down = down.ewm(com = periods - 1, adjust=True, min_periods = periods).mean()
+
+    rsi = ma_up / ma_down
+    rsi = 100 - (100/(1 + rsi))
+    return rsi
+
+
+# Compute the Bollinger Bands 
+def BBANDS(data, n=50):
+    MA = data.Close.rolling(window=n).mean()
+    SD = data.Close.rolling(window=n).std()
+    data['MiddleBand'] = MA
+    data['UpperBand'] = MA + (2 * SD) 
+    data['LowerBand'] = MA - (2 * SD)
+    return data
+
+
+def MA(data,n=50):
+    # Simple Moving Average 
+    def SMA(data, ndays): 
+        SMA = pd.Series(data['Close'].rolling(ndays).mean(), name = 'SMA') 
+        data = data.join(SMA) 
+        return data
+
+    # Exponentially-weighted Moving Average 
+    def EWMA(data, ndays): 
+        EMA = pd.Series(data['Close'].ewm(span = ndays, min_periods = ndays - 1).mean(), 
+                    name = 'EWMA_' + str(ndays)) 
+        data = data.join(EMA) 
+        return data    
+
+    # Compute the 50-day SMA
+    SMA = SMA(data,n)
+    SMA = SMA.dropna()
+    SMA = SMA['SMA']
+
+    # Compute the 200-day EWMA
+    ew = 200
+    EWMA = EWMA(data,ew)
+    EWMA = EWMA.dropna()
+    EWMA = EWMA['EWMA_200']
+    return SMA,EWMA
+
+start_date = dt.datetime.today()- dt.timedelta(3000) 
+end_date = dt.datetime.today()
+stock ="USDJPY=X"
+data = yf.download(stock, start_date, end_date)
+print(len(data))
+rsi=RSI(data)
+mfi=MFI(data)
+bbands=BBANDS(data)
+ma=MA(data)
+sma=ma[0]
+ewma=ma[1]
+print("RSI")
+print(len(rsi))
+print("MFI")
+print(len(mfi))
+print("BBANDS")
+print(len(bbands))
+print("MA")
+print(len(sma))
+print(len(ewma))
+smallest_length_data=len(ewma)
+rsi=rsi[len(rsi)-len(ewma):]
+mfi=mfi[len(mfi)-len(ewma):]
+bbands=bbands[len(bbands)-len(ewma):]
+sma=sma[len(sma)-len(ewma):]
+print("RSI")
+print(len(rsi))
+print(rsi)
+print("MFI")
+print(len(mfi))
+print(mfi)
+print("BBANDS")
+print(len(bbands))
+print(bbands)
+print("MA")
+print(len(sma))
+print(sma)
+print(len(ewma))
+print(ewma)
