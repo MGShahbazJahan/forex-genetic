93395319a20a22d1455f4a1dd904ec3badcbfe22
Shahbaz Jahan <shahbazjahan9@gmail.com>
2024-04-09 16:44:44 +0530

    initial commit

--- project/streamlit_app.py
+++ project/streamlit_app.py
@@ -1,10 +1,16 @@
-import streamlit as st
-import requests 
+"""
+WSGI config for project project.
 
+It exposes the WSGI callable as a module-level variable named ``application``.
 
-def main():
-    response = requests.get('http://127.0.0.1:8000')
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
 
+import os
 
-if __name__ == "__main__":
-    main()
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'project.settings')
+
+application = get_wsgi_application()
